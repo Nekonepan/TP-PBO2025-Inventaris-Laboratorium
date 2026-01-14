0e5932739cc7ea405aa0831b8e736cd6c29d415b
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2026-01-14 23:09:52 +0700

    Refactor Alat class to use database instance in methods and improve parameter handling

--- src/models/alat.py
+++ src/models/alat.py
@@ -1,14 +1,16 @@
 class Alat:
-    def __init__(self, nama, kondisi, status, kategori_id):
-        self.nama = nama
-        self.kondisi = kondisi
-        self.status = status
-        self.kategori_id = kategori_id
+    def __init__(self, db):
+        self.db = db
 
-    def tambah_alat(self, db):
+    def tambah_alat(self, nama, kondisi, status, kategori_id):
         query = """
         INSERT INTO alat (nama_alat, kondisi, status, kategori_id)
         VALUES (?, ?, ?, ?)
         """
-        db.cursor.execute(query, (self.nama, self.kondisi, self.status, self.kategori_id))
-        db.conn.commit()
+        self.db.cursor.execute(query, (nama, kondisi, status, kategori_id))
+        self.db.conn.commit()
+
+    def get_all_alat(self):
+        query = "SELECT alat_id, nama_alat, kondisi, status FROM alat"
+        self.db.cursor.execute(query)
+        return self.db.cursor.fetchall()
